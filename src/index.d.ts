cf80ddb97221db4dae5a5063ec7fa139a962e903
Geoloep <contact@geoloep.nl>
2017-01-31 10:38:24 +0100

    Add typings

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+declare namespace L {
+    export namespace CRS {
+        export const RD: L.CRS;
+    }
+    
+    export namespace Projection {
+        export const RD: L.Projection;
+    }
+}
+
+declare module 'leaflet-rd' {
+    var e: any;
+    export = e;
+}
